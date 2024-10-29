50c5ec356be4981e14fb851dbd0b783135f056b5
joelmarquesmartins25 <joelmarquesmartins25@gmail.com>
2024-10-28 23:30:29 -0300

    Create initial generic test

---
+++ src/VideoUploader.Tests/Domains/Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace VideoUploader.Tests.Domains;
+
+public class Tests
+{
+    [Fact]
+    public async Task Test()
+    {
+        // Arrange
+
+        // Act
+
+        // Assert
+
+        await Task.CompletedTask;
+    }
+}
